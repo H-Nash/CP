d8ca44fc2e7615b32133403cee38f41fdcbfdacf
H-Nash <98226667+H-Nash@users.noreply.github.com>
2024-04-10 12:47:24 -0500

    Create BlockLetters.py

---
+++ Career Path's/Computer Science/BlockLetters.py
@@ -0,0 +1,24 @@
+#Nathan Hershel
+#I play Drums
+
+firstName = '''
+N    N
+NN   N
+N N  N
+N  N N
+N   NN
+N    N
+N    N
+'''
+
+lastName ='''
+H   H
+H   H
+H   H
+HHHHH
+H   H
+H   H
+H   H
+'''
+
+print(firstName + lastName)
