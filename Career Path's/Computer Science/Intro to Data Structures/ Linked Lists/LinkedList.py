b17067a398392969482cf8cda46eb4659f146350
H-Nash <98226667+H-Nash@users.noreply.github.com>
2024-04-10 15:45:15 -0500

    Create LinkedList.py

---
+++ Career Path's/Computer Science/Intro to Data Structures/ Linked Lists/LinkedList.py
@@ -0,0 +1,40 @@
+# Linked List Implementation
+# Node Class
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+# Linked List
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+
+  # Sets new node with a new value as the head node and sets the previous headnode as its link
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+
+  # Crawls through linked list and turns the output into a string 
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
